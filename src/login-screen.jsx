407ed7ad170279af6ba860cef64652c27925ce00
Reddy Basha Naguru <reddy.naguru@myntra.com>
2020-06-28 19:17:25 +0530

    initial commit

---
+++ src/login-screen.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LoginComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>Inside login component</p>
+            </div>
+        )
+    }
+}
+
+export default LoginComponent;
